9c144e8102796a1057cd54cb55148ff2312c73da
darknighten <hi-bai-du@163.com>
2015-11-13 12:21:45 +0800

    加入了阴影效果

--- site/Task_5/js/menu.js
+++ site/Task_5/js/menu.js
@@ -6,6 +6,8 @@ function hidebar () {
 	document.getElementById('mask').style.display = "none";
 	document.getElementById('menu').style.display = "none";
 	document.getElementById('main').style.transform = "";
+	document.getElementById('main').style.boxShadow = "";
+
 }
 function showBar () {
 	document.getElementById('mask').style.transition = "all 1s";
@@ -13,6 +15,7 @@ function showBar () {
 	document.getElementById('menu').style.display = "block";
     document.getElementById('main').style.transition = "all 0.3s";
     document.getElementById('main').style.transform = "translate(70vw,13.5vw)";
+    document.getElementById('main').style.boxShadow = "0px 0px 10px #888888";
 }
 
 function handle () {
