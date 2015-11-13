7583e62fdafbb95cc35880333beeb4af3d8c684f
darknighten <hi-bai-du@163.com>
2015-11-13 08:25:21 +0800

    菜单的js

---
+++ site/Task_5/js/menu.js
@@ -0,0 +1,21 @@
+var flag = 0;
+document.getElementById('menuButton').onclick = function(evt) {
+	handle();
+}
+function hidebar () {
+	document.getElementById('mask').style.display = "none";
+	document.getElementById('menu').style.display = "none";
+	document.getElementById('main').style.transform = "";
+}
+function showBar () {
+	document.getElementById('mask').style.transition = "all 1s";
+	document.getElementById('mask').style.display = "block";
+	document.getElementById('menu').style.display = "block";
+    document.getElementById('main').style.transition = "all 0.3s";
+    document.getElementById('main').style.transform = "translate(70vw,13.5vw)";
+}
+
+function handle () {
+    flag = !flag;
+    flag==0 ? hidebar() : showBar();
+}
