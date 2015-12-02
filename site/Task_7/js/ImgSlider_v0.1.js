1243be9845f36837e0f9aec5c9929f4fa845e1b1
darknighten <hi-bai-du@163.com>
2015-12-02 19:56:27 +0800

    添加了page control

--- site/Task_7/js/ImgSlider_v0.1.js
+++ site/Task_7/js/ImgSlider_v0.1.js
@@ -14,39 +14,23 @@ function ImgSlider (_width, _totalHeight, _imgArray) {
 }
 //初始化组件
 ImgSlider.prototype.init = function() {
-	//从width和数组长度，计算出总宽度 totalWidth;
 	this.totalWidth = this.width*this.imgArray.length; 
-	//计算步进量：width/totalWidth
 	this.step = (this.width/this.totalWidth)*100;
 	this.a = 0;
 	this.index = 1;
-	//（_width为每张图片容器的宽度，图片要在其中水平垂直居中的，超出部分hidden）
-	//（_totalheight为每张图片容器的高度，同时也是总的轮播图的高度,图片要在其中水平垂直居中，超出的部分hidden）
-	//
-	//拿到ul，将totalWidth赋给其 width 属性；
+
 	var ul = document.getElementById('imgSlider-container').getElementsByTagName('ul')[0];
     console.log(ul);
 	ul.style.width = this.totalWidth + "px";
-	//拿到ul，将totalHeight赋给其 height 属性；
 	ul.style.height = this.totalHeight + "px";
-	//拿到ul，设置display: block;
 	ul.style.display = "block";
-	//拿到ul，设置position: absolute;
-	ul.style.position = "absolute";
-	//拿到ul，插入webkitTransition = "0.5s";
 	ul.style.webkitTransition = "0.5s";
-	//拿到ul，插入-webkit-transform: translate(0,0);
 	ul.style.webkitTransform = "translate(0,0)";
-	//
-	//拿到li，将width赋给其 width 属性；
-	//拿到li，将totalHeight赋给其 height 属性；
-	//拿到li，设置float: left;
-	//拿到li，设置display: inline;
-	//拿到li，设置list-style-type: none;
+
 	var li = document.getElementById('imgSlider-container').getElementsByTagName('li');
 	console.log(li.length);
 	var i;
-	for (i=0; i<=3; i++) {
+	for (i=0; i<=(this.imgArray.length-1); i++) {
 		li[i].style.height = this.totalHeight + "px";
 		li[i].style.width = this.width + "px";
 		li[i].style.float = "left";
@@ -56,21 +40,36 @@ ImgSlider.prototype.init = function() {
 	}
 }
 //轮播函数
-ImgSlider.prototype.start = function() {
-	//判断当前的索引index，不等于最后一张图的话就继续+步进量；超过最后一张的就修改偏移值为0，同时还原其他相关变量;
-	console.log("->轮播开始" );
-	var ul = document.getElementById('imgSlider-container').getElementsByTagName('ul')[0];
-	if (this.index<this.imgArray.length) {
+ImgSlider.prototype.start = function(_direction) {
+	this.direction = _direction;
+	var crr;
+	var nxt;
+	this.crr = document.getElementById('pageControl-bottom').getElementsByTagName('div')[this.index-1];
+	if (this.direction == "left") {
 		this.a = this.a+this.step;
-		ul.style.webkitTransform = "translate(-" + this.a + "%,0)";
 		this.index++;
-		console.log("  跳转完成");
+	} else if (this.direction == "right") {
+		this.a = this.a-this.step;
+		this.index--;
+	}
+	var ul = document.getElementById('imgSlider-container').getElementsByTagName('ul')[0];
+	if (this.index<(this.imgArray.length+1)&&this.index>1) {
+		ul.style.webkitTransform = "translate(-" + this.a + "%,0)";
+		this.nxt = document.getElementById('pageControl-bottom').getElementsByTagName('div')[this.index-1];
+	} else if (this.index<1) {
+		ul.style.webkitTransform = "translate(-" + (this.step*(this.imgArray.length-1)) + "%,0)";
+		this.a = this.step*(this.imgArray.length-1);
+		this.index = this.imgArray.length;
+		this.nxt = document.getElementById('pageControl-bottom').getElementsByTagName('div')[this.index-1];
 	} else {
 		ul.style.webkitTransform = "translate(0,0)";
 		this.index = 1;
 		this.a = 0;
-		console.log("  返回原始位置" );
+		this.nxt = document.getElementById('pageControl-bottom').getElementsByTagName('div')[this.index-1];
 	}
+	this.crr.classList.remove("pageControl-items-current");
+	this.nxt.classList.add("pageControl-items-current");
+	this.crr = this.nxt;
 }
 //创建一个ImgSlider实例对象
 function creatImgSlider(_width, _height, _imgArray) {
